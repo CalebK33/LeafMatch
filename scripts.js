e128b0fe570da43fef9e1ba611823433806ee978
HayleyM11 <hayley_murphy@paraparaumucollege.school.nz>
2025-05-08 15:17:15 +1200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -95,7 +95,7 @@ function takePhoto() {
         flash.style.opacity = 1;
     setTimeout(() => {
         flash.style.opacity = 0;
-    }, 200);
+    }, 100);
 
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
