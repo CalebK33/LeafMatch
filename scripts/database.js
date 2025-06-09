faeb6e47a82437595846df9b42fa767d6e07ed88
Caleb <caleb_keenan@paraparaumucollege.school.nz>
2025-06-10 09:41:44 +1200

    Update database.js

--- scripts/database.js
+++ scripts/database.js
@@ -3,19 +3,19 @@ const id = urlParams.get("ID");
         
 const database = {
   "1": {
-    title: "John Doe",
+    title: "Golden Acrea Palm",
     name: "Ioannes Doe",
     description: "Plant one is a really cool plant for now...",
     image: "images/plants/plant1.jpg",
   },
   "2": {
-    title: "Jane Doe",
+    title: "Cestrum Nocturnum",
     name: "Ioanna Doe",
     description: "Plant two is a really fantastic and interesting plant :)",
     image: "images/plants/plant2.jpg",
   },
   "3": {
-    title: "Unidentified Individual",
+    title: "Sacred Fig",
     name: "Incognita Singula",
     description: "Plant three is exactly the same as plant two! It is not a different plant",
     image: "images/plants/plant2.jpg",
