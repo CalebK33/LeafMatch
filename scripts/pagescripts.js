56827843a0ede570add872fb917a6bb9a3c43e97
Caleb <caleb_keenan@paraparaumucollege.school.nz>
2025-06-10 16:05:29 +1200

    Update pagescripts.js

--- scripts/pagescripts.js
+++ scripts/pagescripts.js
@@ -9,3 +9,18 @@ function closeSidebar() {
 if (!navigator.userAgent.match(/Android|iPhone|iPod|BlackBerry|Windows Phone/i)) {
     document.getElementById("sidebar").style.width = "33%"
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
