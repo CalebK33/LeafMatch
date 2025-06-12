c7dbb5af82ed441a2b24f652338052c3115bc026
Caleb <caleb_keenan@paraparaumucollege.school.nz>
2025-06-13 11:27:23 +1200

    Update database.js

--- scripts/database.js
+++ scripts/database.js
@@ -6,19 +6,16 @@ const database = {
     title: "Golden Acrea Palm",
     name: "Ioannes Doe",
     description: "Plant one is a really cool plant for now...",
-    image: "images/plants/plant1.jpg",
   },
   "2": {
     title: "Cestrum Nocturnum",
     name: "Ioanna Doe",
     description: "Plant two is a really fantastic and interesting plant :)",
-    image: "images/plants/plant2.jpg",
   },
   "3": {
     title: "Sacred Fig",
     name: "Incognita Singula",
     description: "Plant three is exactly the same as plant two! It is not a different plant",
-    image: "images/plants/plant2.jpg",
   },
 };
 
@@ -27,7 +24,7 @@ if (entry) {
   document.getElementById("title").textContent = entry.title;
   document.getElementById("name").textContent = entry.name;
   document.getElementById("description").textContent = entry.description;
-  document.getElementById("image").src = entry.image;
+  document.getElementById("image").src = "images/plant/plant" + id + ".png";
 } else {
   document.getElementById("title").textContent = "Not Found";
   document.getElementById("title").textContent = "This plant does't seem to exist...";
