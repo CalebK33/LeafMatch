0e972696faf34f69a8f426faee13b749842dee2e
Goldnosey <James_Patterson@paraparaumucollege.school.nz>
2025-06-12 16:01:26 +1200

    Update desktopscripts.js

--- scripts/desktopscripts.js
+++ scripts/desktopscripts.js
@@ -175,6 +175,8 @@ function retakePhoto() {
     tick.style.display = "none";
     cross.style.display = "none";
 
+    document.getElementById('fileInput').value = '';
+
     if (camerasavailable === 0 || blocked === 1) {
         nocamera.style.display = '';
     }
