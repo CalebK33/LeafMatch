3275c4fa794e1773e40b5a69da5570279b0f117a
Caleb <caleb_keenan@paraparaumucollege.school.nz>
2025-07-16 13:46:18 +1200

    Update ai.js

--- scripts/ai.js
+++ scripts/ai.js
@@ -62,7 +62,7 @@ function postprocess(data, confidence) {
   const loader = document.querySelector('.loader');
   const loadingscreen = document.querySelector('.loadingscreen');
   alert(confidence)
-  if (confidence > 65) {
+  if (confidence > 75) {
     if (loader && loadingscreen) {
       loader.style.transition = 'opacity 0.35s';
       loader.style.opacity = '0';
